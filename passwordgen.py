aa072a0220413a64e50e4486b68449ca7a195d63
Noarex <cskoksal@outlook.com>
2024-11-04 23:55:53 +0300

    Update passwordgen.py

--- passwordgen.py
+++ passwordgen.py
@@ -1,9 +1,34 @@
 import random
 import pyperclip
+
 text = '1234567890QWERTYUIOP[]\ASDFGHJKLZXCVBNM./qwertyuiopasdfghjklzxcvbnm;:<>?{}#%!()_-=+'
 alphabet = list(text)
 list = random.sample(alphabet, k=20)
 password = ''.join(list)
+
 print("heres your password:", password)
-pyperclip.copy(password)
-input()
+
+retry = input('do you want to retry? y/n ')
+
+if retry == 'y':
+    print("heres your password:", password)
+
+    copy = input('do you want to copy it? y/n ')
+    if copy == 'y':
+        pyperclip.copy(password)
+        print('copied it.')
+        input('Press enter to quit.')
+        quit()
+    else:
+        input('Press enter to quit.')
+        quit()
+else:
+    copy = input('do you want to copy it? y/n ')
+    if copy == 'y':
+        pyperclip.copy(password)
+        print('copied it.')
+        input('Press enter to quit.')
+        quit()
+    else:
+        input('Press enter to quit.')
+        quit()
