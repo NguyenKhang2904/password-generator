b291cbbdd131b697eb01ad042bc0133a8d415ec4
Nguyen Khang <khangb2205987@student.ctu.edu.vn>
2024-11-05 19:44:41 +0700

    This is the new interface for your app.

--- passwordgen.py
+++ passwordgen.py
@@ -1,34 +1,43 @@
 import random
 import pyperclip
+import customtkinter as ctk
+from tkinter import messagebox
 
-text = '1234567890QWERTYUIOP[]\ASDFGHJKLZXCVBNM./qwertyuiopasdfghjklzxcvbnm;:<>?{}#%!()_-=+'
-alphabet = list(text)
-list = random.sample(alphabet, k=20)
-password = ''.join(list)
+# Initialize customtkinter theme
+ctk.set_appearance_mode("System")  # Use system theme (light or dark)
+ctk.set_default_color_theme("blue")  # Default theme
 
-print("Here's your password:", password)
+# Function to generate a password
+def generate_password():
+    text = '1234567890QWERTYUIOP[]\\ASDFGHJKLZXCVBNM./qwertyuiopasdfghjklzxcvbnm;:<>?{}#%!()_-=+'
+    alphabet = list(text)
+    password = ''.join(random.sample(alphabet, k=20))
+    password_var.set(password)
 
-retry = input('Do you want to retry? y/n ')
+# Function to copy password to clipboard
+def copy_password():
+    pyperclip.copy(password_var.get())
+    messagebox.showinfo("Copied", "Password copied to clipboard!")
 
-if retry == 'y':
-    print("Here's your password:", password)
+# Setting up the modern GUI
 
-    copy = input('Do you want to copy it? y/n ')
-    if copy == 'y':
-        pyperclip.copy(password)
-        print('Copied.')
-        input('Press enter to quit.')
-        quit()
-    else:
-        input('Press enter to quit.')
-        quit()
-else:
-    copy = input('Do you want to copy it? y/n ')
-    if copy == 'y':
-        pyperclip.copy(password)
-        print('Copied.')
-        input('Press enter to quit.')
-        quit()
-    else:
-        input('Press enter to quit.')
-        quit()
+# App title
+root = ctk.CTk()
+root.title("Password Generator")
+root.geometry("400x250")
+
+password_var = ctk.StringVar()
+
+# Password display and buttons
+ctk.CTkLabel(root, text="Generated Password:", font=("Arial", 16)).pack(pady=10)
+password_entry = ctk.CTkEntry(root, textvariable=password_var, font=("Arial", 14), width=280)
+password_entry.pack(pady=5)
+
+generate_button = ctk.CTkButton(root, text="Generate Password", command=generate_password, width=200, height=40)
+generate_button.pack(pady=10)
+
+copy_button = ctk.CTkButton(root, text="Copy Password", command=copy_password, width=200, height=40)
+copy_button.pack(pady=5)
+
+# Start the GUI loop
+root.mainloop()
